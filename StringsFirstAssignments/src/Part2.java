64517aaacbadb2e33dd33480cbc34cfaefb14939
Mayank Solan <mayanksolan@users.noreply.github.com>
2018-09-18 16:05:53 +0530

    Part2 and Part3

--- StringsFirstAssignments/src/Part2.java
+++ StringsFirstAssignments/src/Part2.java
@@ -1,11 +1,11 @@
 
 public class Part2 {
-	public static String findSimpleGene(String dna) {
-		int indatg = dna.indexOf("ATG");
+	public static String findSimpleGene(String dna, String startCodon, String stopCodon) {
+		int indatg = dna.toUpperCase().indexOf(startCodon);
 		if(indatg == -1) {
 			return "";
 		}
-		int indtaa = dna.indexOf("TAA",indatg);
+		int indtaa = dna.toUpperCase().indexOf(stopCodon,indatg);
 		if (indtaa == -1) {
 			return "";
 		}
@@ -18,15 +18,25 @@ public class Part2 {
 	}
 	public static void testSimpleGene()
 	{
+		String startCodon = "ATG";
+		String stopCodon = "TAA";
 		String a1 = "PTACCCTAA";
 		String a2 = "ATGCCCPTA";
 		String a3 = "PTACCCPTA";
-		String a4 = "ATGCCCNNNRRRTTTTAA";
+		String a4 = "TPACPDATGCCCNNNRRRTTTTAATPACPD";
 		String a5 = "ATGCCCNNNRTTTTAA";
-		System.out.println("a1 = " + findSimpleGene(a1));
-		System.out.println("a2 = " + findSimpleGene(a2));
-		System.out.println("a3 = " + findSimpleGene(a3));
-		System.out.println("a4 = " + findSimpleGene(a4));
-		System.out.println("a5 = " + findSimpleGene(a5));
+		String a6 = "ATGGGTTAA";
+		String a7 = "gatgctataat";
+		System.out.println("a1 = " + findSimpleGene(a1,startCodon,stopCodon));
+		System.out.println("a2 = " + findSimpleGene(a2,startCodon,stopCodon));
+		System.out.println("a3 = " + findSimpleGene(a3,startCodon,stopCodon));
+		System.out.println("a4 = " + findSimpleGene(a4,startCodon,stopCodon));
+		System.out.println("a5 = " + findSimpleGene(a5,startCodon,stopCodon));
+		System.out.println("a6 = " + findSimpleGene(a6,startCodon,stopCodon));
+		System.out.println("a7 = " + findSimpleGene(a7,startCodon,stopCodon));
+	}
+	public static void main(String[] args)
+	{
+		testSimpleGene();
 	}
 }
