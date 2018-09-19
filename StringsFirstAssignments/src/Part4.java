4f5f694a98eeccbe0f366a0e410a3174896133f7
Mayank Solan <mayanksolan@users.noreply.github.com>
2018-09-19 06:45:30 +0530

    part4

---
+++ StringsFirstAssignments/src/Part4.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+public class Part4 {
+	public static void main(String[] args) throws Exception
+	  {
+	    String urlString = "http://www.dukelearntoprogram.com/course2/data/manylinks.html";
+	    URL url = new URL(urlString);
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+	    String line;
+	    while ((line = reader.readLine()) != null)
+	    {
+	    	//System.out.println(line);
+	    	int ytind = line.toUpperCase().indexOf("YOUTUBE.COM");
+	    	if (ytind != -1)
+	    	{
+	    		int dqfirst = line.lastIndexOf("\"",ytind);
+	    		int dqsecond = line.indexOf("\"",ytind);
+	    		if(dqfirst != -1 && dqsecond != -1)
+	    			System.out.println("URL is " + line.substring(dqfirst+1, dqsecond));
+	    	}
+	    }
+	    reader.close();
+	  }
+}
