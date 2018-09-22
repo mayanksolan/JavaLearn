7d5d9de34aaedf2b2b75747a293b5a61eaa0dbaa
Mayank Solan <mayanksolan@users.noreply.github.com>
2018-09-22 17:53:15 +0530

    part3 countgene

--- StringsSecondAssignments/src/Part3.java
+++ StringsSecondAssignments/src/Part3.java
@@ -12,10 +12,6 @@ public class Part3 {
 		}
 		else return -1;
 	}
-	/**
-	 * @param dna
-	 * @return
-	 */
 	public static String findGene(String dna)
 	{
 		int startIndex = dna.toUpperCase().indexOf("ATG");
@@ -27,7 +23,7 @@ public class Part3 {
 			int endIndexTAA = findStopCodon(dna,startIndex,"TAA");						
 			int endIndexTAG = findStopCodon(dna,startIndex,"TAG");
 			int endIndexTGA = findStopCodon(dna,startIndex,"TGA");
-			System.out.println("TAA "+endIndexTAA+" TAG "+endIndexTAG+" TGA "+endIndexTGA);
+			//System.out.println("TAA "+endIndexTAA+" TAG "+endIndexTAG+" TGA "+endIndexTGA);
 			if (endIndexTAA == -1 || (endIndexTGA != -1 && endIndexTGA < endIndexTAA))
 				minIndex = endIndexTGA ; 
 			else
@@ -40,22 +36,43 @@ public class Part3 {
 		else
 			return "";
 	}
+	public static int countGenes(String dna)
+	{
+		int currInd = 0 ; int count = 0;
+		System.out.println("DNA String - " + dna + " results into ");
+		while (currInd != dna.length())
+		{
+			String set = findGene(dna.substring(currInd));
+			if (set != "") {
+				System.out.println(set);
+				int setIndex = dna.indexOf(set);
+				int setLen = set.length();
+				currInd = setIndex + setLen;
+				count++;
+			}
+		}
+		return count;
+	}
 	public static void testFindStopCodon()
 	{
-		/*String a1 = "ATGjvbrtgTAA";
+		String a1 = "ATGjvbrtgTAA";
 		String a2 = "ABATGhgdtvnTAA";
 		String a3 = "ABATGhgdgbtvnTTA";
 		String a4 = "ABATGfghtreTAGyhrTAA";
 		String a5 = "ABATGfghtrTAGyhrTAA";
-		String a6 = "ABATGfghtrTAGyhTAAaaTGA";*/
+		String a6 = "ABATGfghtrTAGyhTAAaaTGA";
 		String a7 = "ABATGfghtprTAGyhbATGbntmtpTAAaabATGnbtbevverTGA";
-		/*System.out.println("DNA String - " + a1 + " results into " + findStopCodon(a1,0,"TAA"));
+		String a8 = "ATGTAGATGTAAATGTGA";
+		System.out.println("DNA String - " + a1 + " results into " + findStopCodon(a1,0,"TAA"));
 		System.out.println("DNA String - " + a2 + " results into " + findStopCodon(a2,2,"TAA"));
 		System.out.println("DNA String - " + a3 + " results into " + findStopCodon(a3,2,"TTA"));
 		System.out.println("DNA String - " + a4 + " results into " + findGene(a4));
 		System.out.println("DNA String - " + a5 + " results into " + findGene(a5));
-		System.out.println("DNA String - " + a6 + " results into " + findGene(a6));*/
+		System.out.println("DNA String - " + a6 + " results into " + findGene(a6));
 		printAllGenes(a7);
+		System.out.println("DNA String - " + a7 + " has " + countGenes(a7));
+		printAllGenes(a8);
+		System.out.println("DNA String - " + a8 + " has " + countGenes(a8));
 	}
 	public static void printAllGenes(String dna)
 	{
